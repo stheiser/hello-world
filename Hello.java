62f3f6b295c2ec8c96bbe2706624b83a6f3b0540
Hizo! <stheiser@tempeunion.org>
2020-07-22 11:44:54 -0700

    Add files via upload
    Testing

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*
+ * Student Name
+ * Period
+ * Short description of the program
+ * 
+ * Stephen Heiser
+ * P3
+ * 
+ * This program will output the words "Hello World"
+ */
+public class Hello 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" ///////");
+		System.out.println("@  * *  @");
+		System.out.println("||  >  ||");
+		System.out.println("||\\ - /||");
+		System.out.println("||  +  ||");
+		System.out.println(" )      (");
+		//System.out.print("6th Period Drools!");
+	}
+
+}
